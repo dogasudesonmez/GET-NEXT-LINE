b21c37904470827aed2eb0cca1bbe8e5528c205e
Doğa Sude Sönmez <82324445+dogasudesonmez@users.noreply.github.com>
2024-10-30 01:25:11 +0300

    Add files via upload

---
+++ Get Next Line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsonmez <dsonmez@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 00:55:22 by dsonmez           #+#    #+#             */
+/*   Updated: 2024/10/30 00:56:27 by dsonmez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
